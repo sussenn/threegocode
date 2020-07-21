91172f2657e95c96d0b7e6832254e39812dbe6de
sussenn <sussenn@qq.com>
2020-07-21 23:38:24 +0800

    互斥锁及管道笔记

--- c03资源竞争的发现race及互斥锁使用/main/start.go
+++ c03资源竞争的发现race及互斥锁使用/main/start.go
@@ -6,7 +6,12 @@ import (
 )
 
 //计算1-200的各个数的和，并且把各个数的和放入到map中
-//报错concurrent map writes.		全局变量未加锁,发生资源争夺
+//问题1: 报错concurrent map writes.		全局变量未加锁,发生资源争夺. 即多个协程同时并发写map
+//go build -race start.go	(-race 找出程序中发生资源竞争的数据)
+//解决1: 使用互斥锁 或者	[使用管道channel存储数据]
+
+//问题2: 协程还在执行,而主线程已执行完毕,终止了程序
+//解决2: 使用管道channel
 var (
 	myMap = make(map[int]int, 10)
 	//声明一个全局的互斥锁 lock
@@ -17,6 +22,8 @@ var (
 func test(n int) {
 	res := 1
 	for i := 1; i <= n; i++ {
+		//如果这里使用阶乘,数值过大会超过int长度,则输出 res = 0
+		//res *= i
 		res += i
 	}
 	//加锁
@@ -31,9 +38,10 @@ func main() {
 		//开启协程 执行test()函数
 		go test(i)
 	}
-	//休眠10s
+	//休眠10s	等待协程执行完毕
 	//time.Sleep(time.Second * 10)
 
+	//这里加锁是因为即便程序声明等待10s,而底层代码不会理解,仍然发生资源竞争
 	lock.Lock() //加锁
 	//输出myMap
 	for i, v := range myMap {
